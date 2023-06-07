8d9a22cd0408ff523602a5e203c7ba4e416267c1
Julian Gomez <julgomez14@gmail.com>
2023-06-06 22:49:32 -0500

    create basic structure of the project

---
+++ src/test/java/seleniumgluecode/Test.java
@@ -0,0 +1,23 @@
+package seleniumgluecode;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Test {
+  @Given("^El usuario se encuentra en la categoria Home de imalittletester$")
+  public void el_usuario_se_encuentra_en_la_categoria_Home_de_imalittletester() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+  }
+
+  @When("^Hace click sobre el boton The little tester comics$")
+  public void hace_click_sobre_el_boton_The_little_tester_comics() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+  }
+
+  @Then("^Se debe redirigir a la pantalla Comics$")
+  public void se_debe_redirigir_a_la_pantalla_Comics() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+  }
+}
