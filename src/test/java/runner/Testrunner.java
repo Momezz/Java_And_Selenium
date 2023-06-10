f7ff098d6c47a973736228dc1a30081589e0b0ac
Julian Gomez <julgomez14@gmail.com>
2023-06-10 18:27:06 -0500

    I create reports in the browser

--- src/test/java/runner/Testrunner.java
+++ src/test/java/runner/Testrunner.java
@@ -2,13 +2,27 @@ package runner;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import org.junit.AfterClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 
 @CucumberOptions(
         features = "src/test/java/features",
-        glue = {"seleniumgluecode"}
+        glue = {"seleniumgluecode"},
+        plugin = {"json:test/report/cucumber_report.json"}
 )
 public class Testrunner {
+
+    @AfterClass
+    public static void finish(){
+        try {
+            System.out.println("Generando reporte");
+            String[] cmd = {"cmd.exe", "/c", "npm run report"};
+            Runtime.getRuntime().exec(cmd);
+            System.out.println("Reporte generado satisfactoriamente");
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
